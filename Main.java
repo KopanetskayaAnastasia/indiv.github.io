419079fd504cb204333c2de5791ca44f2432584e
KopanetskayaAnastasia <112947763+KopanetskayaAnastasia@users.noreply.github.com>
2022-10-31 10:37:58 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,38 +2,20 @@ import java.util.*;
 public class Main {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        int r,k;
-      /* System.out.println("Enter count of money:");
-        r=sc.nextInt();
-        k=sc.nextInt();
-        Money m=new Money(r,k);
-        System.out.println("Enter count of other money:");
-        r= sc.nextInt();
-        k= sc.nextInt();
-        Money m1 =new Money(r,k);
-        System.out.println("Enter course of some currency:");
-        r= sc.nextInt();
-        k= sc.nextInt();
-        Money course=new Money(r,k);
-        System.out.println("If the exchange rate is in rubles - 1, No - 2");
-        int c= sc.nextInt();
-        if (c==1)
-            m.division(course.rub, course.kop);
-        else m.multiplication(course.rub, course.kop);
-        m.difference(m1.rub, m1.kop);
-        m.sum(m1.rub, m1.kop);*/
-
-        System.out.println("Enter Fraction:");
-        r= sc.nextInt();
-        k= sc.nextInt();
-        Fraction f=new Fraction(r,k);
-        System.out.println("Enter other Fraction:");
-        r= sc.nextInt();
-        k= sc.nextInt();
-        Fraction f1=new Fraction(r,k);
-        f.sum(f1.x, f1.y);
-        f.difference(f1.x, f1.y);
-        f.multiplication(f1.x, f1.y);
-        f.division(f1.x, f1.y);
+        System.out.println("Enter date:");
+        int d= sc.nextInt();
+        int m=sc.nextInt();
+        int y=sc.nextInt();
+        try{
+            Date date=new Date(d,m,y);
+            date.printDate();
+            date.number();
+            System.out.println("Enter days for sum:");
+            int q= sc.nextInt();
+            Date date1=date.sum(q);
+        }
+        catch(MyException e){
+            System.out.println(e.getMessage()+" "+e.getDate());
+        }
     }
 }
